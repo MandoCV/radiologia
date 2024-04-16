148d82cdcf7a1795537824ac7167d93d8d911c18
MandoCV <112577507+MandoCV@users.noreply.github.com>
2024-04-15 22:25:13 -0600

    Edicion de NavBar Style

---
+++ Front End/src/components/xray/navbars/NavBarStyle1.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- TOP Nav Bar -->
+  <div class="iq-top-navbar header-top-sticky">
+    <div class="iq-navbar-custom" :class="horizontal ? 'd-flex align-items-center justify-content-between' : ''">
+      <div class="iq-sidebar-logo">
+        <div class="top-logo">
+          <router-link :to="homeURL">
+            <img :src="logo" class="img-fluid" alt="logo" />
+            <span>XRay</span>
+          </router-link>
+        </div>
+      </div>
+      <div class="iq-menu-horizontal" v-if="horizontal">
+        <div class="iq-sidebar-menu">
+          <List :items="items" class="d-flex"></List>
+        </div>
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-light p-0">
+
+
+        <b-collapse id="nav-collapse" is-nav>
+          <slot name="responsiveRight" />
+        </b-collapse>
+        <slot name="right" />
+      </nav>
+    </div>
+  </div>
+  <!-- TOP Nav Bar END -->
+</template>
+
+<style scoped>
+.mini-menu {
+  display: flex;
+}
+
+.mini-menu-item {
+  margin-right: 20px; /* Ajusta el margen derecho */
+}
+</style>
+
+<script>
+import List from '../menus/ListStyle1'
+import SideBarItems from '../../../FackApi/json/SideBar'
+export default {
+  name: 'NavBarStyle1',
+  props: {
+    homeURL: { type: Object, default: () => ({ name: 'dashboard' }) },
+    title: { type: String, default: 'Dashboard' },
+    logo: { type: String, default: require('../../../assets/images/logo.png') },
+    horizontal: { type: Boolean, default: false },
+    items: { type: Array }
+  },
+  components: {
+    List
+  },
+  data() {
+    return {
+      sidebar: SideBarItems,
+      globalSearch: '',
+      showSearch: false,
+      showMenu: false,
+      showMiniMenu: false
+    }
+  }
+}
+</script>
